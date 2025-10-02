d9bb20c5e2cc1f8ff6945c141dd9f13cb42376f7
flypt7 <tomsarrazin01@gmail.com>
2025-10-02 14:30:56 -0400

    implemented retrieve method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,7 +88,19 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
+        // path provided is empty
+        if (path == null) {
+            return this.root;
+        }
+
+        else if (path.charAt(0) == 'l') {
+            return this.left.retrieve(path.substring(1));
+        }
+
+        else if (path.charAt(0) == 'r') {
+            return this.right.retrieve(path.substring(1));
+        }
+
         return -1;
     }
 
